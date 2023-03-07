67ebd83f18efb4940af2ae3c56f0d949c08c2285
Jahnreil Amarillento <jjamarillento@mymail.mapua.edu.ph>
2023-03-08 02:26:03 +0800

    Fully working, complete requirements from LE3

--- AmarillentoLE3/BlogDataLibrary/Data/SqlData.cs
+++ AmarillentoLE3/BlogDataLibrary/Data/SqlData.cs
@@ -15,7 +15,7 @@ namespace BlogDataLibrary.Data
     {
         private ISqlDataAccess _db;
         private const string connectionStringName = "SqlDb";
-
+        // owner: Jahnreil Amarillento
         public SqlData(ISqlDataAccess db)
         {
             _db = db;
@@ -27,6 +27,26 @@ namespace BlogDataLibrary.Data
             UserModel result = _db.LoadData<UserModel, dynamic>("BlogDB.dbo.spUsers_Authenticate", new { username, password }, connectionStringName, true).FirstOrDefault();
             
             return result;
+        }
+
+        public void Register(string username, string firstName, string lastName, string password)
+        {
+            _db.SaveData<dynamic>("BlogDB.dbo.spUsers_Register",new { username, firstName, lastName, password }, connectionStringName, true);
+        }
+
+        public void AddPost(PostModel post)
+        {
+            _db.SaveData("BlogDB.dbo.spPosts_Insert", new { post.UserId, post.Title, post.Body, post.DateCreated }, connectionStringName, true);
+        }
+
+        public List<ListPostModel> ListPosts()
+        {
+            return _db.LoadData<ListPostModel, dynamic>("BlogDB.dbo.spPosts_List", new { }, connectionStringName, true).ToList();
+        }
+
+        public ListPostModel ShowPostDetails(int id)
+        {
+            return _db.LoadData<ListPostModel, dynamic>("BlogDB.dbo.spPosts_Detail", new {id}, connectionStringName, true).FirstOrDefault();
         }
     }
 }
--- AmarillentoLE3/BlogTestUI/Program.cs
+++ AmarillentoLE3/BlogTestUI/Program.cs
@@ -9,12 +9,111 @@ class Program
 
     static void Main(string[] args)
     {
-        SqlData db = GetConnection();
-       
-        Authenticate(db);
+        
+        bool repeat = true;
+        int MenuInput = 0;
+        bool loggedIn = false;
+        do {
+            
+            SqlData db = GetConnection();
+            
+            
+            try
+            {
+                int tempMenuInputint = 0;
 
-        Console.WriteLine("Press Enter to exit...");
-        Console.ReadLine();
+                Console.Write(
+                "\tC# .NET 6 Class libraries demonstration, please type the number based on the given menu [1-6]\n" +
+                "[1] Login\n" +
+                "[2] Register\n" +
+                "[3] Add post\n" +
+                "[4] Show Posts\n" +
+                "[5] Show Detailed post\n" +
+                "[6] Exit\n" +
+                "-------------\n" +
+                "Enter menu number: "
+                );
+
+
+                string tempMenuInputstr = Console.ReadLine();
+                tempMenuInputint = int.Parse(tempMenuInputstr);
+
+                if (tempMenuInputint < 1 || tempMenuInputint > 6)
+                {
+                    throw new Exception();
+                }
+
+                MenuInput = tempMenuInputint;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong value entered, please try again");
+                
+            }
+
+            switch (MenuInput)
+            {
+                case 1:
+                    Console.Clear();
+                    Authenticate(db);
+                    Console.WriteLine("====================\n");
+                    loggedIn = true;
+
+                    break;
+                case 2:
+                    Register(db);
+                    Console.Clear();
+                    Console.WriteLine("Sucessfully Registered!");
+                    break;
+                case 3:
+                   
+                    if (loggedIn = false)
+                    {
+                        Authenticate(db);
+                        loggedIn = true;
+                    }
+                    else 
+                    {
+                        AddPost(db);
+                    }
+                    Console.Clear();
+                    break;
+                case 4:
+                    if (loggedIn = false)
+                    {
+                        Authenticate(db);
+                        loggedIn = true;
+                    }
+                    else
+                    {
+                        ListPosts(db);
+                    }
+                    break;
+                case 5:
+                    if (loggedIn = false)
+                    {
+                        Authenticate(db);
+                        loggedIn = true;
+                    }
+                    else
+                    {
+                        ShowPostDetails(db);
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Exiting program");
+                    repeat = false;
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+
+            }
+        }
+        
+        while (repeat) ;
     }
 
     static SqlData GetConnection()
@@ -50,10 +149,81 @@ class Program
         if (user == null)
         {
             Console.WriteLine("Invalid credentials.");
-        } else
+        }
+        else
         {
             Console.WriteLine($"Welcome, {user.UserName}");
         }
     }
+
+    public static void Register(SqlData db)
+    {
+        Console.Write("Enter new username: ");
+        var username = Console.ReadLine();
+
+        Console.Write("Enter new password: ");
+        var password = Console.ReadLine();
+
+        Console.Write("Enter first name: ");
+        var firstName = Console.ReadLine();
+
+        Console.Write("Enter last name: ");
+        var lastName = Console.ReadLine();
+
+        db.Register(username, firstName, lastName, password);
+
+    }
+
+    private static void AddPost(SqlData db)
+    {
+        UserModel user = GetCurrentUser(db);
+
+        Console.Write("Title: ");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Write body: ");
+        string body = Console.ReadLine();
+
+        PostModel post = new PostModel
+        {
+            Title = title,
+            Body = body,
+            DateCreated = DateTime.Now,
+            UserId = user.Id
+        };
+
+        db.AddPost(post);
+    }
+
+    private static void ListPosts(SqlData db)
+    {
+        List<ListPostModel> posts = db.ListPosts();
+
+        foreach (ListPostModel post in posts)
+        {
+            Console.WriteLine($"{post.Id}. Title: {post.Title} by {post.UserName} [{post.DateCreated.ToString("yyyy-MM-dd")}");
+            Console.WriteLine($"{post.Body.Substring(0, 20)}...\n");
+
+        }
+    }
+
+    private static void ShowPostDetails(SqlData db)
+    {
+        Console.Write("Enter a post ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+
+        ListPostModel post = db.ShowPostDetails(id);
+        Console.WriteLine(post.Title);
+        Console.WriteLine($"by {post.FirstName} {post.LastName} [{post.UserName}]\n");
+
+        Console.WriteLine(post.Body);
+        Console.WriteLine(post.DateCreated.ToString("MMM d yyyy"));
+
+
+    }
+
 }
+
+
+
 
