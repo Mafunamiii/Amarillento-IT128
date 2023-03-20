da50f218be8987fb2cc03fe090883886d55961d8
Jahnreil Amarillento <jjamarillento@mymail.mapua.edu.ph>
2023-03-20 19:51:11 +0800

    Updated CreateItem and UpdateItem requests

--- M1Partial/Products.UI/ProductsAPI/ProductsAPI/Controllers/itemController.cs
+++ M1Partial/Products.UI/ProductsAPI/ProductsAPI/Controllers/itemController.cs
@@ -21,5 +21,33 @@ namespace ItemAPI.Controllers
     {
       return Ok(await _context.Items.ToListAsync());
     }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Item>>> CreateItem(Item item)
+    {
+      _context.Items.Add(item);
+      await _context.SaveChangesAsync();
+
+      return Ok(await _context.Items.ToListAsync());
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<List<Item>>> UpdateItem(Item item)
+    {
+      var dbItem = await _context.Items.FindAsync(item.id);
+      if (dbItem == null)
+        return BadRequest("Item not found.");
+
+      dbItem.id = item.id;
+      dbItem.name = item.name;
+      dbItem.code = item.code;
+      dbItem.brand = item.brand;
+      dbItem.unitprice = item.unitprice;
+
+      await _context.SaveChangesAsync();
+
+      return Ok(await _context.Items.ToListAsync());
+
+    }
   }
 }
